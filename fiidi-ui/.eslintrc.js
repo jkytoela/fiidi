9298748edb2991eb6f14b24ca69c6408e2503644
Jaakko Kytölä <kytola.jaakko@gmail.com>
2023-01-06 13:48:27 +0200

    setup fiidi-ui
    - setup React/TS/Vite/ESLint with sensible defaults
    - dockerize fiidi-ui

---
+++ fiidi-ui/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        'avoidEscape': true,
+        'allowTemplateLiterals': true
+      }
+    ]    
+  },
+};
